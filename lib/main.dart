02c6ca11ce7018257798bea0224c3e720052d557
suekiaoi <sueki.aoi@gmail.com>
2021-05-17 06:38:07 +0900

    現在地表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,56 +32,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  LocationData currentLocation;
+
+  // StreamSubscription<LocationData> locationSubscription;
+
+  Location _locationService = new Location();
+  String error;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initPlatformState();
+    _locationService.onLocationChanged.listen((LocationData result) async {
+      setState(() {
+        currentLocation = result;
+      });
+    });
+  }
+
+  void initPlatformState() async {
+    LocationData myLocation;
+    try {
+      myLocation = await _locationService.getLocation();
+      error = "";
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENITED')
+        error = 'Permission denited';
+      else if (e.code == 'PERMISSION_DENITED_NEVER_ASK')
+        error =
+            'Permission denited - please ask the user to enable it from the app settings';
+      myLocation = null;
+    }
+    setState(() {
+      currentLocation = myLocation;
+    });
+  }
 
   void _takePhoto() {
     setState(() {
-      _counter++;
+      //TODO: 写真撮影
     });
   }
 
   Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kTarget = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: _kGooglePlex,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-          }),
-      floatingActionButton: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(left:30.0 ,bottom:10.0),
-              child: FloatingActionButton.extended(
-                onPressed: _takePhoto,
-                label: Text('ゴミみっけ'),
-                tooltip: '撮影',
-                icon: Icon(Icons.camera_alt),
-              ),
-            ),
-          ],
+    if (currentLocation == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(currentLocation.latitude, currentLocation.longitude),
+            zoom: 17.0,
+          ),
+          myLocationEnabled: true,
+        ),
+        floatingActionButton: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(left: 30.0, bottom: 10.0),
+                child: FloatingActionButton.extended(
+                  onPressed: _takePhoto,
+                  label: Text('ゴミみっけ'),
+                  tooltip: '撮影',
+                  icon: Icon(Icons.camera_alt),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
